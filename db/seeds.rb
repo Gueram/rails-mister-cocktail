a5b9d471ab5e7a1b68a65052ff5cb8c3b1ca5801
Gueram Minassian <gueram.minassian@gmail.com>
2020-11-13 12:05:03 +0100

    Cocktail_creations_in_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,38 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 require 'open-uri'
+require 'faker'
 
+puts "Cleaning database..."
+Dose.destroy_all
 Ingredient.destroy_all
+Cocktail.destroy_all
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 ingredients_serialized = open(url).read
 ingredients = JSON.parse(ingredients_serialized)
 
+puts "Creating ingredients..."
 ingredients['drinks'].each do |ingredient|
   Ingredient.create(name: ingredient["strIngredient1"])
 end
+
+puts "Creating cocktails..."
+cocktail = Cocktail.create(name: "Mojito" )
+puts "Created #{cocktail.name}"
+cocktail = Cocktail.create(name: "Espresso martini" )
+puts "Created #{cocktail.name}"
+cocktail = Cocktail.create(name: "Pimm’s" )
+puts "Created #{cocktail.name}"
+cocktail = Cocktail.create(name: "Passion fruit martini" )
+puts "Created #{cocktail.name}"
+cocktail = Cocktail.create(name: "Pina colada" )
+puts "Created #{cocktail.name}"
+cocktail = Cocktail.create(name: "Rum punch" )
+puts "Created #{cocktail.name}"
+cocktail = Cocktail.create(name: "Cosmopolitan" )
+puts "Created #{cocktail.name}"
+cocktail = Cocktail.create(name: "Aperol spritz" )
+puts "Created #{cocktail.name}"
 
 puts "Finished!"
